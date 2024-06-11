c9702262e52dd424d80377a3415db8e84716d60b
Felipe Fragoso <58962669+felipeofragoso@users.noreply.github.com>
2024-06-11 18:34:43 -0300

    up code

---
+++ src/test/java/com/senac/neki/CollaboratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.neki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaboratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
